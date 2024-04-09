dc46c06705513dee8451d53b5f2915cd9120e8ad
Aaron Garcia <axyscreativeagency@gmail.com>
2024-04-08 20:29:21 -0600

    folder structure clean up

--- javascript/index.js
+++ javascript/index.js
@@ -8,26 +8,4 @@ import "./theme.js";
 import "./platform.js";
 import "./magnet.js";
 import "./mouse-cursor.js";
-
-// Clear focus from any element (except inputs) on mousemove
-(function clearFocusOnMouseMove() {
-  function removeFocus() {
-    if (
-      document.activeElement &&
-      !document.activeElement.classList.contains("input")
-    ) {
-      document.activeElement.blur();
-    }
-  }
-  // Set up event listeners
-  document.addEventListener("mousemove", removeFocus);
-})();
-
-// Get Current Year for Copyright
-(function getCurrentYear() {
-  const yearText = document.querySelector(".year-text");
-  const currentYear = new Date().getFullYear();
-
-  yearText.innerHTML = currentYear;
-  yearText.setAttribute("datetime", currentYear);
-})();
+import "./utility.js";
